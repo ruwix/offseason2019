c151e9f8049b5492c9b5094844e0831f3b578803
ruwix <grantpauker@gmail.com>
2019-03-12 13:49:49 -0700

    Implement trajectory methods, add hermitespline methods,  update path

--- src/auto/hermitespline.py
+++ src/auto/hermitespline.py
@@ -19,7 +19,7 @@ class HermiteSpline:
         self.dy = np.empty(self.length)
         self.computeCoefficients()
 
-    def interpolatePoint(self, t):
+    def getPoint(self, t):
         """Interpolate a point along the spline where 0 <= t <= self.length."""
         assert 0 <= t <= self.length
         index = int(np.floor(t))
@@ -38,7 +38,7 @@ class HermiteSpline:
         )
         return np.array([x, y])
 
-    def interpolateDerivative(self, t):
+    def getD(self, t):
         """Interpolate the derivative along the spline where 0 <= t <= self.length."""
         assert 0 <= t <= self.length
         index = int(np.floor(t))
@@ -47,35 +47,89 @@ class HermiteSpline:
         dy = (3 * self.ay[index] * t ** 2) + (2 * self.by[index] * t) + (self.cy[index])
         return np.array([dx, dy])
 
-    def interpolate2ndDerivative(self, t):
-        assert 0 <= t <= self.length
+    def getDD(self, t):
         """Interpolate the 2nd derivative along the spline where 0 <= t <= self.length."""
+        assert 0 <= t <= self.length
         index = int(np.floor(t))
         t -= index
         ddx = (6 * self.ax[index] * t) + (2 * self.bx[index])
         ddy = (6 * self.ay[index] * t) + (2 * self.by[index])
         return np.array([ddx, ddy])
 
-    def interpolateCurvature(self, t):
+    def getDDD(self, t):
+        """Interpolate the 3rd derivative along the spline where 0 <= t <= self.length."""
         assert 0 <= t <= self.length
-        """Interpolate the curvature along the spline where 0 <= t <= self.length."""
-        dx, dy = self.interpolateDerivative(t)
-        ddx, ddy = self.interpolate2ndDerivative(t)
-        return (dx * ddy - dy * ddx) / ((dx * dx + dy * dy) * np.hypot(dx, dy))
+        index = int(np.floor(t))
+        t -= index
+        dddx = 6 * self.ax[index]
+        dddy = 6 * self.ay[index]
+        return np.array([dddx, dddy])
 
-    def interpolateHeading(self, t):
-        """Interpolate a the angle of the tangent along the spline where 0 <= t <= self.length."""
-        dx, dy = self.interpolateDerivative(t)
+    def getCurvature(self, t):
+        """Interpolate the curvature along the spline where 0 <= t <= self.length."""
+        assert 0 <= t <= self.length
+        dx, dy = self.getD(t)
+        ddx, ddy = self.getDD(t)
+        dx2dy2 = dx ** 2 + dy ** 2
+        return (dx * ddy - dy * ddx) / (dx2dy2 ** 1.5)
+
+    def getRadius(self, t):
+        """Interpolate the curvature along the spline where 0 <= t <= self.length."""
+        c = self.getCurvature(t)
+        if c == 0:
+            return float("inf")
+        else:
+            return 1 / self.getCurvature(t)
+
+    def getDCurvature(self, t):
+        assert 0 <= t <= self.length
+        """Interpolate the derivative of the curvature along the spline where 0 <= t <= self.length."""
+        dx, dy = self.getD(t)
+        ddx, ddy = self.getDD(t)
+        dddx, dddy = self.getDDD(t)
+        dx2dy2 = dx ** 2 + dy ** 2
+        dc = (dx * dddy - dy * dddx) / (dx2dy2 ** 1.5) - 3 * (dx * ddy - dy * ddx) * (
+            dx * ddx + dy * ddy
+        ) / (dx2dy2 ** 2.5)
+        return dc
+
+    def getHeading(self, t):
+        """Interpolate the angle of the tangent along the spline where 0 <= t <= self.length."""
+        dx, dy = self.getD(t)
         return np.rad2deg(np.arctan2(dy, dx))
 
-    def interpolatePose(self, t):
-        """Interpolate a pose on the spline where 0 <= t <= self.length."""
-        points = self.interpolatePoint(t)
-        heading = self.interpolateHeading(t)
+    def getPose(self, t):
+        """Interpolate the pose on the spline where 0 <= t <= self.length."""
+        points = self.getPoint(t)
+        heading = self.getHeading(t)
         return np.append(points, heading)
 
+    def getLinearVelocity(self, t):
+        """Interpolate the linear velocity of a particle traveling along the spline where 0 <= t <= self.length."""
+        dx, dy = self.getD(t)
+        return np.hypot(dx, dy)
+
+    def getAngularVelocity(self, t):
+        """Interpolate the angular velocity of a particle traveling along the spline where 0 <= t <= self.length."""
+        omega = self.getLinearVelocity(t) / self.getRadius(t)
+        return -np.rad2deg(omega)
+
+    def getTwist(self, t):
+        """Interpolate the twist of a particle traveling along the spline at timestamp t where 0 <= t <= self.length."""
+        v = self.getLinearVelocity(t)
+        omega = self.getAngularVelocity(t)
+        return np.append(v, omega)
+
+    def getArcLength(self, sample_size=0.01):
+        arc_length = 0
+        for i in range(0, self.length * int(1 / sample_size) - 1):
+            x0, y0 = self.getPoint(i * sample_size)
+            x1, y1 = self.getPoint((i + 1) * sample_size)
+            arc_length += np.hypot(x1 - x0, y1 - y0)
+        return arc_length
+
     def computeCoefficients(self):
-        """Compute the coefficients of the spline equations. This must be called inorder to make interpolations."""
+        """Compute the coefficients of the spline. This must be called in order to make interpolations."""
         for i in range(0, self.length):
             scale = 2 * np.hypot(
                 self.poses[i + 1][0] - self.poses[i][0],
@@ -102,15 +156,15 @@ class HermiteSpline:
             self.ay[i] = (
                 self.dy0[i]
                 + self.dy1[i]
-                + 2 * self.poses[i][0]
+                + 2 * self.poses[i][1]
                 - 2 * self.poses[i + 1][1]
             )
             self.by[i] = (
                 -2 * self.dy0[i]
                 - self.dy1[i]
-                - 3 * self.poses[i][0]
+                - 3 * self.poses[i][1]
                 + 3 * self.poses[i + 1][1]
             )
             self.cy[i] = self.dy0[i]
-            self.dy[i] = self.poses[i][0]
+            self.dy[i] = self.poses[i][1]
 
--- src/auto/trajectory.py
+++ src/auto/trajectory.py
@@ -1,30 +1,67 @@
 import numpy as np
-from csv import reader
+from csv import reader, writer
 from auto.hermitespline import HermiteSpline
-
-
-def loadPath(file):
-    with open(file, "r") as path:
-        _reader = reader(path)
-        headings = next(_reader)
-        assert headings == ["x", "y", "heading"]
-        ret = np.array(list(_reader)).astype(float)
-        path.close()
-        return ret
+from pyfrc.sim import get_user_renderer
 
 
 class Trajectory:
-    def __init__(self, poses, dt):
+    def __init__(self, poses, time, sample_size=0.02):
         self.path = HermiteSpline(poses)
         self.poses = np.empty((0, 3))
-        self.velocities = np.empty((0, 3))
-        self.dt = dt
+        self.velocities = np.empty((0, 2))
+        self.sample_size = sample_size
+        self.time = time
+        self.timestamp = 0
+
+    @staticmethod
+    def loadPath(file):
+        with open(file, "r") as path:
+            _reader = reader(path)
+            headings = next(_reader)
+            assert headings == ["x", "y", "heading"]
+            ret = np.array(list(_reader)).astype(float)
+            path.close()
+            return ret
+
+    def getAverageVelocity(self):
+        return self.path.getArcLength() / self.time
+
+    def update(self, t):
+        self.timestamp = t
+
+    def getState(self):
+        if not self.isFinished():
+            pose = np.round(
+                self.path.getPose(self.timestamp / (self.time / self.path.length)), 2
+            )
+            twist = np.round(
+                self.path.getTwist(self.timestamp / (self.time / self.path.length))
+                / self.time,
+                2,
+            )
+            return np.append(pose, twist)
+        else:
+            return None
 
     def build(self):
-        np.set_printoptions(suppress=True)
-        for i in range(0, self.path.length * 100):
-            pose = self.path.interpolatePose(i / 100).reshape((1, 3))
+        for i in range(0, int(self.path.length / self.sample_size)):
+            pose = np.round(self.path.getPose(i * self.sample_size).reshape((1, 3)), 2)
+            twist = np.round(
+                self.path.getTwist(i * self.sample_size).reshape((1, 2)) / self.time, 2
+            )
             self.poses = np.append(self.poses, pose, axis=0)
-        for i in range(0, len(self.poses) - 1):
-            velocity = ((self.poses[i + 1] - self.poses[i]) / self.dt).reshape((1, 3))
-            self.velocities = np.append(self.velocities, velocity, axis=0)
+            self.velocities = np.append(self.velocities, twist, axis=0)
+
+    def isFinished(self):
+        return self.timestamp >= self.time
+
+    def writeCSV(self, filename):
+        with open(filename, mode="w") as output:
+            writer_ = writer(output, delimiter=",", quotechar='"')
+            writer_.writerow(["x", "y", "heading", "v", "omega"])
+            data = np.concatenate((self.poses, self.velocities), axis=1)
+            writer_.writerows(data)
+            output.close()
+
+    def drawSimulation(self):
+        get_user_renderer().draw_line(self.poses[:, 0:2], scale=(1 / 12, 1 / 12))
