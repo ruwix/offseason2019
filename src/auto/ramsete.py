16c6963f569af690a002b44c13d895c35b95cdd5
ruwix <grantpauker@gmail.com>
2019-03-15 21:16:32 -0700

    Create error getter

--- src/auto/ramsete.py
+++ src/auto/ramsete.py
@@ -6,11 +6,12 @@ class Ramsete:
     def __init__(self, kbeta: float, kzeta: float):
         self.kbeta = kbeta  # 0 < kbeta; larger kbeta makes convergence more aggresive
         self.kzeta = kzeta  # 0 < kzeta < 1; larger kbeta provides more damping
+        self.error = RobotState()
 
     def update(self, state: RobotState, state_d: RobotState) -> Twist:
         # Compute errors
-        error = state_d - state
-        error.heading = boundHalfRadians(error.heading)
+        self.error = state_d - state
+        self.error.heading = boundHalfRadians(self.error.heading)
         # Compute trig functions
         cos_theta = np.cos(state.heading)
         sin_theta = np.sin(state.heading)
@@ -19,17 +20,19 @@ class Ramsete:
         k2 = self.kbeta
         k3 = k1
         # Find linear velocity
-        v = state_d.v * np.cos(error.heading) + k1 * (
-            error.x * cos_theta + error.y * sin_theta
+        v = state_d.v * np.cos(self.error.heading) + k1 * (
+            self.error.x * cos_theta + self.error.y * sin_theta
         )
         # Find angular velocity
         omega = (
             state_d.omega
             + k2
             * state_d.v
-            * np.sinc(error.heading)
-            * (error.y * cos_theta - error.x * sin_theta)
-            + k3 * error.heading
+            * np.sinc(self.error.heading)
+            * (self.error.y * cos_theta - self.error.x * sin_theta)
+            + k3 * self.error.heading
         )
         return Twist(v, omega)
 
+    def getError(self):
+        return self.error
