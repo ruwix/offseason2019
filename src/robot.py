74e4cdd4d7610546a6c4611fb7f91777526d901a
ruwix <grantpauker@gmail.com>
2019-03-22 17:53:34 -0700

    Create lazytalon util

--- src/robot.py
+++ src/robot.py
@@ -6,55 +6,49 @@ from components.chassis import Chassis
 from components.autoselector import AutoSelector
 from autonomous.trajectory import Trajectory
 from autonomous.ramsete import Ramsete
+from utils.lazytalonsrx import LazyTalonSRX
 import numpy as np
 
 
 class Robot(magicbot.MagicRobot):
     chassis: Chassis
     autoselector: AutoSelector
-
     VELOCITY_KP = 0.0
     VELOCITY_KI = 0.0
     VELOCITY_KD = 0.0
     VELOCITY_KF = 0.0
-    KBETA = 2.0
+    KBETA = 20.0
     KZETA = 0.7
 
     def createObjects(self):
         np.set_printoptions(suppress=True)
         """Create motors and stuff here"""
-        self.drive_motor_left = ctre.WPI_TalonSRX(1)
-        self.drive_motor_right = ctre.WPI_TalonSRX(2)
+        self.drive_motor_left = LazyTalonSRX(1)
+        self.drive_motor_right = LazyTalonSRX(2)
 
-        # for motor in (self.drive_motor_left, self.drive_motor_right):
-        self.drive_motor_left.configSelectedFeedbackSensor(
-            ctre.FeedbackDevice.QuadEncoder, 0, 0
+        self.drive_motor_left.initialize(
+            inverted=False, encoder=True, phase=False, name="Drive Motor Left"
         )
-        self.drive_motor_left.selectProfileSlot(0, 0)
-        self.drive_motor_left.config_kP(0, self.VELOCITY_KP)
-        self.drive_motor_left.config_kI(0, self.VELOCITY_KI)
-        self.drive_motor_left.config_kD(0, self.VELOCITY_KD)
-        self.drive_motor_left.config_kF(0, self.VELOCITY_KF)
-        self.drive_motor_right.configSelectedFeedbackSensor(
-            ctre.FeedbackDevice.QuadEncoder, 0, 0
+        self.drive_motor_right.initialize(
+            inverted=False, encoder=True, phase=False, name="Drive Motor Right"
         )
-        self.drive_motor_right.selectProfileSlot(0, 0)
-        self.drive_motor_right.config_kP(0, self.VELOCITY_KP)
-        self.drive_motor_right.config_kI(0, self.VELOCITY_KI)
-        self.drive_motor_right.config_kD(0, self.VELOCITY_KD)
-        self.drive_motor_right.config_kF(0, self.VELOCITY_KF)
+        self.drive_motor_left.setPIDF(
+            0, self.VELOCITY_KP, self.VELOCITY_KI, self.VELOCITY_KD, self.VELOCITY_KF
+        )
+        self.drive_motor_right.setPIDF(
+            0, self.VELOCITY_KP, self.VELOCITY_KI, self.VELOCITY_KD, self.VELOCITY_KF
+        )
+
         self.imu = ctre.PigeonIMU(3)
 
         self.driver = wpilib.joystick.Joystick(0)
         self.operator = wpilib.joystick.Joystick(1)
-        self.timer = wpilib.Timer()
 
         self.ramsete = Ramsete(self.KBETA, self.KZETA)
 
     def teleopInit(self):
         """Called when teleop starts; optional"""
-        self.timer.reset()
-        self.timer.start()
+        pass
 
     def teleopPeriodic(self):
         """Called on each iteration of the control loop"""
---
+++ src/utils/lazytalonsrx.py
@@ -0,0 +1,117 @@
+import ctre
+import logging
+from networktables import NetworkTables
+
+
+class LazyTalonSRX(ctre.WPI_TalonSRX):
+    """A wraper for the ctre.WPI_TalonSRX to simplfy configuration and getting/setting values."""
+
+    MotorDash = NetworkTables.getTable("SmartDashboard").getSubTable("TalonSRX")
+    ControlMode = ctre.WPI_TalonSRX.ControlMode
+
+    def __init__(self, id):
+        super().__init__(id)
+
+    def initialize(self, inverted=False, encoder=False, phase=False, name=None):
+        """Initialize the motors (enable the encoder, set invert status, set voltage limits)."""
+        self.encoder = encoder
+        if self.encoder:
+            self.configSelectedFeedbackSensor(
+                ctre.FeedbackDevice.QuadEncoder, 0, timeoutMs=10
+            )
+            self.setSensorPhase(phase)
+        self.setInverted(inverted)
+        if name != None:
+            self.setName(name)
+        self.no_encoder_warning = f"No encoder connected to {self.name}"
+        self.no_closed_loop_warning = f"{self.name} not in closed loop mode"
+
+    def setPIDF(self, slot, kp, ki, kd, kf):
+        """Initialize the PIDF controller."""
+        self.selectProfileSlot(slot, 0)
+        self.config_kP(slot, kp, 0)
+        self.config_kI(slot, ki, 0)
+        self.config_kD(slot, kd, 0)
+        self.config_kF(slot, kf, 0)
+
+    def setMotionMagicConfig(self, vel, accel):
+        self.configMotionAcceleration(int(accel), 0)
+        self.configMotionCruiseVelocity(int(vel), 0)
+
+    def setPercentOutput(self, signal, max_signal=1):
+        """Set the percent output of the motor."""
+        signal = min(max(signal, -max_signal), max_signal)
+        self.set(self.ControlMode.PercentOutput, signal)
+
+    def setPositionSetpoint(self, pos):
+        """Set the position of the motor."""
+        self.set(self.ControlMode.Position, pos)
+
+    def setVelocitySetpoint(self, vel):
+        """Set the velocity of the motor."""
+        self.set(self.ControlMode.Velocity, vel)
+
+    def setMotionMagicSetpoint(self, pos):
+        """Set the position of the motor using motion magic."""
+        self.set(self.ControlMode.MotionMagic, pos)
+
+    def zero(self, pos=0):
+        """Zero the encoder if it exists."""
+        if self.encoder:
+            self.setSelectedSensorPosition(pos, 0, 0)
+        else:
+            logging.warning(self.no_encoder_warning)
+
+    def getPosition(self):
+        """Get the encoder position if it exists."""
+        if self.encoder:
+            return self.getSelectedSensorPosition(0)
+        else:
+            logging.warning(self.no_encoder_warning)
+            return 0
+
+    def getVelocity(self):
+        """Get the encoder velocity if it exists."""
+        if self.encoder:
+            return self.getSelectedSensorVelocity(0)
+        else:
+            logging.warning(self.no_encoder_warning)
+            return 0
+
+    def getError(self):
+        """Get the closed loop error if in closed loop mode."""
+        if self._isClosedLoop():
+            return self.getClosedLoopError(0)
+        else:
+            logging.warning(self.no_closed_loop_warning)
+            return 0
+
+    def getTarget(self):
+        """Get the closed loop target if in closed loop mode."""
+        if self._isClosedLoop():
+            return self.getClosedLoopTarget(0)
+        else:
+            logging.warning(self.no_closed_loop_warning)
+            return 0
+
+    def outputToDashboard(self):
+        pass
+        # self.MotorDash.putNumber(
+        #     f"{self.name} Percent Output", self.getMotorOutputPercent()
+        # )
+        # if self.encoder:
+        #     self.MotorDash.putNumber(f"{self.name} Position", self.getPosition())
+        #     if self._isClosedLoop():
+        #         self.MotorDash.putNumber(
+        #             f"{self.name} PIDF Target", self.getClosedLoopTarget(0)
+        #         )
+        #         self.MotorDash.putNumber(
+        #             f"{self.name} PIDF Error", self.getClosedLoopError(0)
+        #         )
+
+    def _isClosedLoop(self):
+        return self.getControlMode() in (
+            ctre.WPI_TalonSRX.ControlMode.Velocity,
+            ctre.WPI_TalonSRX.ControlMode.Position,
+            ctre.WPI_TalonSRX.ControlMode.MotionMagic,
+        )
